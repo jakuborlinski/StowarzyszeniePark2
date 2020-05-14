b7def7c2e35c8fada268cbb11a88becfdcc53536
Jakub Orlinski <orlinski.jakub@gmail.com>
2020-05-14 13:32:56 +0200

    Update index.js
    Sprawdzanie interakcji

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -15,7 +15,7 @@ const NavBar = ({ toggleNavbar, isActive }) => (
       <nav className='navbar is-fixed-top' aria-label='main navigation'>
         <div className='navbar-brand'>
           <Link to='/' className='navbar-item'>
-            <strong>Stowarzyszenie PARK</strong>
+            <strong>Kasia i love U</strong>
           </Link>
           <button
             className={`button navbar-burger ${isActive ? 'is-active' : ''}`}
