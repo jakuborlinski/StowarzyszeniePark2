17dae65b2ee7850b37d7c77f1dd48f5fb3287fba
Jakub Orlinski <orlinski.jakub@gmail.com>
2020-05-14 14:01:48 +0200

    Update config.js
    changes in config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'Stowarzyszenie P.A.R.K', // Site title.
+  siteTitleAlt: 'Strona stoważyszenia które zajmuje się przestrzenią ,aktywnością, rekreacją oraz kulturą.', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Strona poświęcona działalności stowarzyszenia P.A.R.K.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  disqusShortname: 'stowarzyszeni-park', // Disqus shortname.
+  userName: 'Jakub Orlinski',
+  userTwitter: 'none',
+  userLocation: 'Nowa Sol, polska',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © Stowarzyszenie PARK 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
