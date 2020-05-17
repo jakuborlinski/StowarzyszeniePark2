9614a06045b546e8215afe5961978c718ce707d4
Jakub Orlinski <orlinski.jakub@gmail.com>
2020-05-17 15:51:42 +0200

    Jakub
    Dodawanie zakładki hero do przechwytywania danych do hero img

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteTitle: 'Stowarzyszenie P.A.R.K', // Site title.
   siteTitleAlt: 'Strona stoważyszenia które zajmuje się przestrzenią ,aktywnością, rekreacją oraz kulturą.', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://s-park.org', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription: 'Strona poświęcona działalności stowarzyszenia P.A.R.K.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
@@ -11,7 +11,7 @@ module.exports = {
   disqusShortname: 'stowarzyszeni-park', // Disqus shortname.
   userName: 'Jakub Orlinski',
   userTwitter: 'none',
-  userLocation: 'Nowa Sol, polska',
+  userLocation: 'Nowa Sol, Polska',
   userDescription: '',
   copyright: 'Copyright © Stowarzyszenie PARK 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
